2b8008c5bfc6a3fa2a487ccdbccbfb08ba7ee466
Biegh John Paul Alonzo <bieghjohnpaul.alonzo@dxc.com>
2024-10-04 03:05:43 +0000

    List Detail Application

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("student.com.sap.training.ux402.listdetail.ux402listdetail.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
